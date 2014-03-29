6b12c2df8b427d279a86387aa99330558dde9a92
Taryn Sauer <taryn.sauer@gmail.com>
2014-03-29 00:13:05 -0500

    Refactor for efficiency

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,5 @@
 class Sudoku
-   attr_accessor :board, :options, :move_count, :precision_value, :begin_board  
-
+  
   def initialize(puzzle)
      @options = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
      @board = puzzle.split('')
@@ -9,60 +8,53 @@ class Sudoku
   end
 
   def solve!
-    while unsolved? 
-      @begin_board = board.dup
-      board.each_with_index do |value, index|
-        stop_guessing_if_move_made
+    while unsolved?
+      @begin_board = @board.dup
+      @board.each_with_index do |value, index|
         make_move_if_possible(value, index)
       end
-      begin_guessing_if_no_move_made
+      begin_guessing
     end
-    solved_puzzle_message
   end
 
-  def begin_guessing_if_no_move_made
-    if @begin_board == board  ## Begin guessing
+  def begin_guessing
+    if @begin_board == @board ## Begin guessing
       @precision_value += 1
     else 
-      precision_value = 1 ## Stop guessing
-    end
-  end
-
-  def stop_guessing_if_move_made
-    if (precision_value > 1 && (@begin_board != board)) 
-      precision_value = 1 ## Stop guessing after placing move of first guess
+      @precision_value = 1 ## Stop guessing
     end
   end
 
   def make_move_if_possible(value, index)
-    if (open_space?(value) && make_move?(index))
-      place_value_on_board(value, index)
+    moves = possible_moves(index)
+    if moves.empty?
+      @board = @begin_board.dup # Reset board
+      @precision_value = 1
+    elsif (open_space?(value) && make_move?(moves))
+      place_value_on_board(moves, index)
     end
   end
 
-  def place_value_on_board(value, cell_index)
-    if possible_moves(cell_index).empty? # Bad move placed while guessing, can't solve
-       @board = @begin_board.dup # Reset board
-       precision_value = 1
-       solve! #Try guessing again
-    else
-       move = possible_moves(cell_index).shuffle.first
-       @board[cell_index] = move
-       precision_value = 1 
-       @move_count += 1
-    end 
+  def place_value_on_board(moves, index)
+    @board[index] = moves.shuffle.first 
+    @precision_value = 1 
+    @move_count += 1
+  end
+
+  def make_move?(moves)
+    moves.length <= @precision_value  
   end
 
   def row_values(index)
     row_num = row_index(index) 
     beg_i = row_num * 9
     end_i = beg_i + 8 
-    board[beg_i..end_i]
+    @board[beg_i..end_i]
   end
 
   def col_values(index)
     col = []
-    board.each_with_index do |v, i|
+    @board.each_with_index do |v, i|
       if col_index(i) == col_index(index) 
         col << v
       end
@@ -70,12 +62,8 @@ class Sudoku
     col
   end 
 
-  def make_move?(index)
-    possible_moves(index).length <= precision_value  
-  end
-
   def print_board
-    printer = Board.new(board)
+    printer = Board.new(@board)
     puts printer.print_board
   end
 
@@ -85,7 +73,7 @@ class Sudoku
   end
 
   def possible_moves(index)
-    options - all_taken_values(index) 
+    @options - all_taken_values(index) 
   end
 
   def all_taken_values(index)
@@ -100,12 +88,12 @@ class Sudoku
   end
 
   def solved_puzzle_message
-    puts "\nSolved puzzle in #{move_count} moves."
+    puts "\nSolved puzzle in #{@move_count} moves."
     print_board
   end
 
   def unsolved?
-    board.include?("0")
+    @board.include?("0")
   end
 
   def open_space?(value)
@@ -131,7 +119,7 @@ class Sudoku
   def get_box_values_by_box_index(box_index) 
     box = []
     3.times do
-      box += board[box_index..(box_index + 2)]
+      box += @board[box_index..(box_index + 2)]
       box_index += 9
     end
     box
